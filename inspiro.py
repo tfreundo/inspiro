0924992909f08aebe0fd7fee6d94dd5b232ef2ff
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2020-08-26 10:06:47 +0200

    Added background image and bluring effect

--- inspiro.py
+++ inspiro.py
@@ -2,25 +2,46 @@ from tkinter import *
 from util.csv_reader import CsvReader
 from util.config_helper import ConfigHelper
 import random
+import cv2
+import PIL.Image
+import PIL.ImageTk
 
 config_helper = ConfigHelper("config.json")
 
 window = Tk()
-window.geometry(config_helper.get_window_geometry()) 
+window.geometry(config_helper.get_window_geometry())
 window.title(config_helper.get_window_title())
+window.resizable(False, False)
 window.configure(bg=config_helper.get_window_background_color())
+
+# Load an image using OpenCV
+bg_img = cv2.cvtColor(cv2.imread(
+    config_helper.get_window_background_image()), cv2.COLOR_BGR2RGB)
+# Get the image dimensions (OpenCV stores image data as NumPy ndarray)
+bg_img_h, bg_img_w, bg_img_no_channels = bg_img.shape
+# Resize the image
+bg_img = cv2.resize(bg_img, (config_helper.get_window_geometry_width(
+), config_helper.get_window_geometry_height()), interpolation=cv2.INTER_AREA)
+bg_img_h, bg_img_w, bg_img_no_channels = bg_img.shape
+# Add bluring of the image if configured
+if config_helper.get_window_blur_background_image():
+    factor = config_helper.get_window_blur_background_image_factor()
+    bg_img = cv2.blur(bg_img, (factor, factor))
+
+canvas = Canvas(window, width=bg_img_w, height=bg_img_h)
+canvas.pack()
+
+bg_img_photoimage = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(bg_img))
+canvas.create_image(0, 0, image=bg_img_photoimage, anchor=NW)
+
 
 quotes = CsvReader.read_quotes(config_helper.get_quotes_filename())
 random_quote_index = random.randint(0, len(quotes)-1)
 quote = quotes[random_quote_index]
 
-# Quote
-quote_txt = Text(window, wrap=WORD, width=300, font=config_helper.get_quote_font_obj(), bg=config_helper.get_window_background_color(), fg=config_helper.get_quotes_foreground_color())
-quote_txt.insert(INSERT, quote.text)
-quote_txt.pack()
-
-# Author (optional)
-if len(quote.author) > 0:
-    quote_txt.insert(END, "\n\n" + quote.author)
+quote_text_w = config_helper.get_quote_textarea_width()
+window_center_x = (bg_img_w/2) - (quote_text_w/2)
+canvas.create_text(window_center_x, 50, text=quote.text + "\n\n" + quote.author, width=quote_text_w,
+                   anchor=NW, fill=config_helper.get_quotes_foreground_color(), font=config_helper.get_quote_font())
 
 window.mainloop()
--- util/config_helper.py
+++ util/config_helper.py
@@ -15,8 +15,23 @@ class ConfigHelper:
     def get_window_geometry(self):
         return self.config["window"]["geometry"]
     
+    def get_window_geometry_width(self):
+        return int(self.get_window_geometry().split("x")[0])
+
+    def get_window_geometry_height(self):
+        return int(self.get_window_geometry().split("x")[1])
+
     def get_window_background_color(self):
         return self.config["window"]["background_color"]
+    
+    def get_window_background_image(self):
+        return self.config["window"]["background_image"]
+
+    def get_window_blur_background_image(self):
+        return self.config["window"]["blur_background_image"]
+
+    def get_window_blur_background_image_factor(self):
+        return self.config["window"]["blur_background_image_factor"]
 
     def get_quotes_filename(self):
         return self.config["quotes"]["quotes_filename"]
@@ -24,20 +39,8 @@ class ConfigHelper:
     def get_quotes_foreground_color(self):
         return self.config["quotes"]["foreground_color"]
 
-    def get_author_font(self):
-        return self.config["quotes"]["author_font"]
-    
-    def get_author_font_size(self):
-        return self.config["quotes"]["author_font_size"]
-
-    def get_author_font_obj(self):
-        return (self.get_author_font(), self.get_author_font_size())
-    
     def get_quote_font(self):
         return self.config["quotes"]["quote_font"]
     
-    def get_quote_font_size(self):
-        return self.config["quotes"]["quote_font_size"]
-    
-    def get_quote_font_obj(self):
-        return (self.get_quote_font(), self.get_quote_font_size())
+    def get_quote_textarea_width(self):
+        return self.config["quotes"]["quote_textarea_width"]
